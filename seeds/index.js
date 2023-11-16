903ec99d4faa0afea4c97abf54b1921cd8ac371b
chris <chriswang.csc@gmail.com>
2023-11-15 21:16:17 -0700

    add basic image

--- seeds/index.js
+++ seeds/index.js
@@ -4,8 +4,6 @@ const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
@@ -20,9 +18,13 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 40) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description: 'N/A',
+      price
     });
     await camp.save();
   }
